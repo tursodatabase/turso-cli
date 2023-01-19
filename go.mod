c2f5a6b2798bfb153a7f17519e63be5e8fe25075
Pekka Enberg <penberg@chiselstrike.com>
2023-01-19 12:17:41 +0200

    Add `db shell <NAME>` command
    Example usage:
    
    ```console
    % turso db shell grown-luckman
    Welcome to Turso SQL shell!
    
    Connected to grown-luckman at http://<...>
    
    →  SELECT * FROM users;
    USERNAME  EMAIL
    alice     alice@example.com
    →  exit
    ```

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 )
 
 require (
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
@@ -24,6 +25,7 @@ require (
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/rodaine/table v1.1.0 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
---
+++ internal/cmd/db_shell.go
@@ -0,0 +1,155 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/chiselstrike/iku-turso-cli/internal/settings"
+	"github.com/chzyer/readline"
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
+	"github.com/spf13/cobra"
+)
+
+func shellArgs(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) == 0 {
+		databases, err := getDatabases()
+		if err != nil {
+			return []string{}, cobra.ShellCompDirectiveNoFileComp
+		}
+		result := make([]string, 0)
+		for _, database := range databases {
+			db := database.(map[string]interface{})
+			name := db["Name"]
+			ty := db["Type"]
+			if ty == "primary" {
+				result = append(result, name.(string))
+			}
+		}
+		return result, cobra.ShellCompDirectiveNoFileComp
+	}
+	return []string{}, cobra.ShellCompDirectiveNoFileComp
+}
+
+var shellCmd = &cobra.Command{
+	Use:               "shell database_name",
+	Short:             "Start a SQL shell.",
+	Args:              cobra.ExactArgs(1),
+	ValidArgsFunction: replicateArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := args[0]
+		if name == "" {
+			return fmt.Errorf("Please specify a database name.")
+		}
+		return runShell(name)
+	},
+}
+
+type QueryRequest struct {
+	Statements []string `json:"statements"`
+}
+
+type QueryResult struct {
+	Results *ResultSet `json:"results"`
+	Error   *Error     `json:"error"`
+}
+
+type ResultSet struct {
+	Columns []string `json:"columns"`
+	Rows    []Row    `json:"rows"`
+}
+
+type Row []interface{}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+func runShell(name string) error {
+	config, err := settings.ReadSettings()
+	if err != nil {
+		return err
+	}
+	dbSettings := config.GetDatabaseSettings(name)
+	promptFmt := color.New(color.FgBlue, color.Bold).SprintFunc()
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:            promptFmt("→  "),
+		HistoryFile:       ".turso_history",
+		InterruptPrompt:   "^C",
+		EOFPrompt:         "exit",
+		HistorySearchFold: true,
+	})
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
+
+	fmt.Printf("Welcome to Turso SQL shell!\n\n")
+	fmt.Printf("Connected to %s at %s\n\n", emph(name), dbSettings.GetURL())
+	for {
+		line, err := l.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+		err = query(dbSettings.GetURL(), line)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func query(url, stmt string) error {
+	rawReq := QueryRequest{
+		Statements: []string{stmt},
+	}
+	req, err := json.Marshal(rawReq)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(url, "application/json", bytes.NewReader(req))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("error: Failed to execute SQL statement %s\n", stmt)
+		return nil
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var results []QueryResult
+	if err := json.Unmarshal(body, &results); err != nil {
+		fmt.Printf("error: Failed to parse response from server: %s\n", err.Error())
+		return err
+	}
+	for _, result := range results {
+		if result.Error != nil {
+			fmt.Printf("error: %s\n", result.Error.Message)
+		}
+		if result.Results != nil {
+			columns := make([]interface{}, 0)
+			for _, column := range result.Results.Columns {
+				columns = append(columns, strings.ToUpper(column))
+			}
+			tbl := table.New(columns...)
+			for _, row := range result.Results.Rows {
+				tbl.AddRow(row...)
+			}
+			tbl.Print()
+		}
+	}
+	return nil
+}
