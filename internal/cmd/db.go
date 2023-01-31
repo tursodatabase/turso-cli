0f0e72fb7fc75819aa04ca66d45213740bb4a1f0
Athos Couto <athos@chiselstrike.com>
2023-01-30 21:10:31 -0300

    Patch replicate cmd to handle both v1 and v2 dbs

--- internal/cmd/db.go
+++ internal/cmd/db.go
@@ -74,6 +74,21 @@ func fetchDatabaseNames() []string {
 	return extractDatabaseNames(databases)
 }
 
+func getDatabaseType(name string) (turso.Database, error) {
+	databases, err := getDatabases()
+	if err != nil {
+		return turso.Database{}, err
+	}
+
+	for _, database := range databases {
+		if database.Name == name {
+			return database, nil
+		}
+	}
+
+	return turso.Database{}, fmt.Errorf("database with name %s not found", name)
+}
+
 func getDatabaseNames() []string {
 	settings, err := settings.ReadSettings()
 	if err != nil {
@@ -375,7 +390,17 @@ var replicateCmd = &cobra.Command{
 			return fmt.Errorf("please login with %s", emph("turso auth login"))
 		}
 		host := getHost()
+
+		original, err := getDatabaseType(name)
+		if err != nil {
+			return fmt.Errorf("please login with %s", emph("turso auth login"))
+		}
+
 		url := fmt.Sprintf("%s/v1/databases", host)
+		if original.Type == "distributed" {
+			url = fmt.Sprintf("%s/v2/databases/%s/instances", host, name)
+		}
+
 		bearer := "Bearer " + accessToken
 		createDbReq := []byte(fmt.Sprintf(`{"name": "%s", "region": "%s", "image": "%s", "type": "replica"}`, name, region, image))
 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(createDbReq))
@@ -408,11 +433,23 @@ var replicateCmd = &cobra.Command{
 		}
 		end := time.Now()
 		elapsed := end.Sub(start)
-		m := result.(map[string]interface{})["database"].(map[string]interface{})
+		var m map[string]interface{}
+		if original.Type == "distributed" {
+			m = result.(map[string]interface{})["instance"].(map[string]interface{})
+		} else {
+			m = result.(map[string]interface{})["database"].(map[string]interface{})
+		}
 		username := result.(map[string]interface{})["username"].(string)
 		password := result.(map[string]interface{})["password"].(string)
-		dbId := m["DbId"].(string)
-		dbHost := m["Hostname"].(string)
+		var dbId, dbHost string
+		fmt.Println(original)
+		if original.Type == "distributed" {
+			dbId = m["Uuid"].(string)
+			dbHost = original.Hostname
+		} else {
+			dbId = m["DbId"].(string)
+			dbHost = m["Hostname"].(string)
+		}
 		fmt.Printf("Replicated database %s to %s in %d seconds.\n\n", emph(name), emph(regionText), int(elapsed.Seconds()))
 		dbSettings := settings.DatabaseSettings{
 			Host:     dbHost,
@@ -459,7 +496,7 @@ var listCmd = &cobra.Command{
 				regionWidth = regionLen
 			}
 		}
-		typeWidth := 7
+		typeWidth := 12
 		fmt.Printf("%-*s  %-*s %-*s  %s\n", nameWidth, "NAME", typeWidth, "TYPE", regionWidth, "REGION", "URL")
 		for _, database := range databases {
 			id := database.ID
