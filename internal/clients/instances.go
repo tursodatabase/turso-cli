6123b331b957e6056c802273ae97704980e8e10d
Athos Couto <athos@chiselstrike.com>
2023-01-31 23:00:46 -0300

    Create instances client with List and Delete

---
+++ internal/clients/instances.go
@@ -0,0 +1,64 @@
+package clients
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type Instance struct {
+	Uuid   string
+	Name   string
+	Type   string
+	Region string
+}
+
+type instances struct {
+	c *client
+}
+
+var Instances = &instances{Turso}
+
+func NewInstances(base *url.URL, token string) *instances {
+	return &instances{NewTurso(base, token)}
+}
+
+func (i *instances) List(db string) ([]Instance, error) {
+	r, err := i.c.Get(fmt.Sprintf("v2/databases/%s/instances", db), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("response with status code %d", r.StatusCode)
+	}
+
+	type ListResponse struct{ Instances []Instance }
+	resp, err := Unmarshall[ListResponse](r)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Instances, nil
+}
+
+func (i *instances) Delete(db, instance string) error {
+	r, err := i.c.Delete(fmt.Sprintf("v2/databases/%s/instances/%s", db, instance), nil)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode == http.StatusBadRequest {
+		body, _ := Unmarshall[struct{ Error string }](r)
+		return errors.New(body.Error)
+	}
+
+	if r.StatusCode != http.StatusOK {
+		return fmt.Errorf("response with status code %d", r.StatusCode)
+	}
+
+	return nil
+}
