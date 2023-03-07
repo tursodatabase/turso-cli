731c7f80a7a57591973bb25aa0f38a33d19f755b
Athos Couto <athos@chiselstrike.com>
2023-03-07 09:53:59 -0300

    Add db update command

---
+++ internal/cmd/db_update.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/chiselstrike/iku-turso-cli/internal/turso"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	dbCmd.AddCommand(dbUpdateCmd)
+	dbUpdateCmd.Flags().BoolVarP(&yesFlag, "yes", "y", false, "Confirms the update of the database.")
+}
+
+func updateArgs(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	if len(args) == 0 {
+		return getDatabaseNames(createTursoClient()), cobra.ShellCompDirectiveNoFileComp
+	}
+	return []string{}, cobra.ShellCompDirectiveNoFileComp
+}
+
+var dbUpdateCmd = &cobra.Command{
+	Use:               "update database_name",
+	Short:             "Updates the database to the latest turso version",
+	Args:              cobra.ExactArgs(1),
+	ValidArgsFunction: updateArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+		client := createTursoClient()
+		name := args[0]
+
+		if _, err := getDatabase(client, name); err != nil {
+			return err
+		}
+
+		if yesFlag {
+			return rotate(client, name)
+		}
+
+		fmt.Printf("To update %s database, all its replicas must be updated.\n", turso.Emph(name))
+		fmt.Printf("All your acitve connections to the DB will be dropped and there will be a short downtime.\n\n")
+
+		ok, err := promptConfirmation("Are you sure you want to do this?")
+		if err != nil {
+			return fmt.Errorf("could not get prompt confirmed by user: %w", err)
+		}
+
+		if !ok {
+			fmt.Println("Database update skipped by the user.")
+			return nil
+		}
+
+		return update(client, name)
+	},
+}
+
+func update(client *turso.Client, name string) error {
+	msg := fmt.Sprintf("Updating database %s", turso.Emph(name))
+	s := startLoadingBar(msg)
+
+	if err := client.Databases.Update(name); err != nil {
+		s.Stop()
+		return fmt.Errorf("error updating database")
+	}
+
+	s.Stop()
+	fmt.Printf("✔  Success! Database %s updated successfully\n", turso.Emph(name))
+	return nil
+}
