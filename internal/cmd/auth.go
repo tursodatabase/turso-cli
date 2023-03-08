2076c6159210587e3fee6feb0b1faef3e34de03f
Pekka Enberg <penberg@chiselstrike.com>
2023-03-08 13:35:41 +0200

    Revert "Merge pull request #261 from WilsonNet/feat/new-login"
    This reverts commit 8723ed0a14b10877a01ce8d778c73b50eede7eac, reversing
    changes made to 0ae78eea5573eadefc1ac48b1547a6cf7db4c05c. Assets are not working.

--- internal/cmd/auth.go
+++ internal/cmd/auth.go
@@ -225,9 +225,7 @@ func createCallbackServer(ch chan string) (*http.Server, error) {
 		ch <- q.Get("username")
 
 		w.WriteHeader(200)
-		tmpl.Execute(w, map[string]string{
-			"assetsURL": getTursoUrl(),
-		})
+		tmpl.Execute(w, q.Get("username"))
 	})
 
 	return &http.Server{Handler: handler}, nil
