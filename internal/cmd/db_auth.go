794a0b5aa0bf3e173c6840cf93dd6eade5992df9
Piotr Jastrzebski <piotr@chiselstrike.com>
2023-03-22 20:42:51 +0100

    Move dbGenerateTokenCmd to db_generatetoken.go
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- internal/cmd/db_auth.go
+++ internal/cmd/db_auth.go
@@ -16,40 +16,10 @@ var dbAuthCmd = &cobra.Command{
 	Short: "Manage database authentication",
 }
 
-var expFlag expirationFlag
-
 func init() {
-	dbAuthCmd.AddCommand(dbGenerateTokenCmd)
 	dbAuthCmd.AddCommand(dbInvalidateTokensCmd)
 
-	usage := "Token expiration. Possible values are 'default' or 'none'."
-	dbGenerateTokenCmd.Flags().VarP(&expFlag, "expiration", "e", usage)
-	dbGenerateTokenCmd.RegisterFlagCompletionFunc("expiration", expirationFlagCompletion)
-
 	dbInvalidateTokensCmd.Flags().BoolVarP(&yesFlag, "yes", "y", false, "Confirms the rotation database credentials.")
-}
-
-var dbGenerateTokenCmd = &cobra.Command{
-	Use:               "generate-token database_name",
-	Short:             "Creates a bearer token to authenticate requests to the database",
-	Args:              cobra.ExactArgs(1),
-	ValidArgsFunction: dbNameArg,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cmd.SilenceUsage = true
-		client := createTursoClient()
-		name := args[0]
-
-		if _, err := getDatabase(client, name); err != nil {
-			return err
-		}
-
-		token, err := client.Databases.Token(name, expFlag.String())
-		if err != nil {
-			return fmt.Errorf("your database does not support token generation")
-		}
-		fmt.Println(token)
-		return nil
-	},
 }
 
 var dbInvalidateTokensCmd = &cobra.Command{
