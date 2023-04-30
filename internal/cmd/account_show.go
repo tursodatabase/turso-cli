ddfb17f2ef4576938329f785b21b9df0f134e76f
Piotr Jastrzebski <piotr@chiselstrike.com>
2023-04-30 18:58:34 +0200

    Parallelize account show
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- internal/cmd/account_show.go
+++ internal/cmd/account_show.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"github.com/chiselstrike/iku-turso-cli/internal/turso"
+	"golang.org/x/sync/errgroup"
 	"time"
 
 	"github.com/chiselstrike/iku-turso-cli/internal"
@@ -41,6 +43,9 @@ var accountShowCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 		// FIXME: this should be done at the server so we can enforce it
+		var dbTokens []string
+		var dbInstances [][]turso.Instance
+		var instanceCount int
 		for _, database := range databases {
 			numLocations += len(database.Regions)
 			instances, err := client.Instances.List(database.Name)
@@ -53,14 +58,34 @@ var accountShowCmd = &cobra.Command{
 				return err
 			}
 
-			for _, instance := range instances {
-				url := getInstanceHttpUrl(settings, &database, &instance)
-				ret, err := inspect(ctx, url, token, instance.Region, false)
-				if err != nil {
-					return err
-				}
-				inspectRet.Accumulate(ret)
+			dbTokens = append(dbTokens, token)
+			instanceCount += len(instances)
+			dbInstances = append(dbInstances, instances)
+		}
+		inspectResCh := make(chan *InspectInfo, instanceCount)
+		g, ctx := errgroup.WithContext(ctx)
+		for idx, database := range databases {
+			idx := idx
+			database := database
+			for _, instance := range dbInstances[idx] {
+				instance := instance
+				g.Go(func() error {
+					url := getInstanceHttpUrl(settings, &database, &instance)
+					ret, err := inspect(ctx, url, dbTokens[idx], instance.Region, false)
+					if err != nil {
+						return err
+					}
+					inspectResCh <- ret
+					return nil
+				})
 			}
+		}
+		if err := g.Wait(); err != nil {
+			return err
+		}
+		for i := 0; i < instanceCount; i++ {
+			ret := <-inspectResCh
+			inspectRet.Accumulate(ret)
 		}
 
 		fmt.Printf("You are currently on %s plan.\n", internal.Emph("starter"))
