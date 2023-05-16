b89fd126f6f39101e54b83255a1cc67437088254
Piotr Jastrzebski <piotr@chiselstrike.com>
2023-05-16 23:15:31 +0200

    Fetch db sizes in parallel
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

--- internal/cmd/db_list.go
+++ internal/cmd/db_list.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
-
 	"github.com/chiselstrike/iku-turso-cli/internal/settings"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
+	"sort"
+	"time"
 )
 
 func init() {
@@ -31,29 +34,48 @@ var listCmd = &cobra.Command{
 			return err
 		}
 
-		data := [][]string{}
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		g, ctx := errgroup.WithContext(ctx)
+		dbInfo := make(chan []string, len(databases))
+
 		for _, database := range databases {
-			url := getDatabaseUrl(settings, &database, false)
-			regions := getDatabaseRegions(database)
+			database := database
+			g.Go(func() error {
+				url := getDatabaseUrl(settings, &database, false)
+				regions := getDatabaseRegions(database)
+				instances, err := client.Instances.List(database.Name)
+				if err != nil {
+					return err
+				}
 
-			instances, err := client.Instances.List(database.Name)
-			if err != nil {
-				return err
-			}
-
-			token, err := client.Databases.Token(database.Name, "1d", true)
-			if err != nil {
-				return err
-			}
-			var size string
-			sizeInfo, err := calculateInstancesUsedSize(instances, settings, database, token)
-			if err != nil {
-				size = fmt.Sprintf("fetching size failed: %s", err)
-			} else {
-				size = sizeInfo.PrintTotal()
-			}
-			data = append(data, []string{database.Name, regions, url, size})
+				token, err := client.Databases.Token(database.Name, "1d", true)
+				if err != nil {
+					return err
+				}
+				var size string
+				sizeInfo, err := calculateInstancesUsedSize(instances, settings, database, token)
+				if err != nil {
+					size = fmt.Sprintf("fetching size failed: %s", err)
+				} else {
+					size = sizeInfo.PrintTotal()
+				}
+				dbInfo <- []string{database.Name, regions, url, size}
+				return nil
+			})
 		}
+
+		if err := g.Wait(); err != nil {
+			return err
+		}
+
+		var data [][]string
+		for range databases {
+			data = append(data, <-dbInfo)
+		}
+		sort.Slice(data, func(i, j int) bool {
+			return data[i][0] > data[j][0]
+		})
 		printTable([]string{"Name", "Locations", "URL", "Size"}, data)
 		settings.SetDbNamesCache(extractDatabaseNames(databases))
 		return nil
