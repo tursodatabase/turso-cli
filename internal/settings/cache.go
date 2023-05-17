tursodatabase/turso-cli

commit d75f14f9def6dd02a4f4c364d2770bb2c74d303d

Refactor cache functions
- Use struct to store data and expiration together
- Use generics to support multiple types

internal/settings/cache.go
@@ -1,0 +1,99 @@
+package settings
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+)
+
+type Entry[T any] struct {
+	Expiration int64 `json:"expiration"`
+	Data       T     `json:"data"`
+}
+
+var ErrExpired = errors.New("cache entry expired")
+
+func cacheKey(key string) string {
+	return "cache." + key
+}
+
+func setCache[T any](key string, ttl int64, value T) error {
+	entry := Entry[T]{Data: value}
+	if ttl > 0 {
+		entry.Expiration = time.Now().Unix() + ttl
+	}
+	viper.Set(cacheKey(key), entry)
+	return nil
+}
+
+func getCache[T any](key string) (T, error) {
+	entry := Entry[T]{}
+	value := viper.Get(cacheKey(key))
+	if err := mapstructure.Decode(value, &entry); err != nil {
+		return entry.Data, fmt.Errorf("failed to get cache data for %s", key)
+	}
+
+	if entry.Expiration != 0 && entry.Expiration < time.Now().Unix() {
+		return entry.Data, ErrExpired
+	}
+
+	return entry.Data, nil
+}
+
+func invalidateCache(key string) error {
+	configMap := viper.AllSettings()
+	delete(configMap, cacheKey(key))
+	encodedConfig, err := json.MarshalIndent(configMap, "", " ")
+	if err != nil {
+		return err
+	}
+	if err := viper.ReadConfig(bytes.NewReader(encodedConfig)); err != nil {
+		return err
+	}
+	return nil
+}
+
+const DB_NAMES_CACHE_KEY = "database_names"
+const DB_NAMES_CACHE_TTL_SECONDS = 30 * 60
+
+func (s *Settings) SetDbNamesCache(dbNames []string) {
+	setCache(DB_NAMES_CACHE_KEY, DB_NAMES_CACHE_TTL_SECONDS, dbNames)
+}
+
+func (s *Settings) GetDbNamesCache() []string {
+	data, err := getCache[[]string](DB_NAMES_CACHE_KEY)
+	if err != nil {
+		return nil
+	}
+	return data
+}
+
+func (s *Settings) InvalidateDbNamesCache() {
+	invalidateCache(DB_NAMES_CACHE_KEY)
+}
+
+const REGIONS_CACHE_KEY = "locations"
+const DEFAULT_REGION_CACHE_KEY = "defaultLocation"
+const REGIONS_CACHE_TTL_SECONDS = 8 * 60 * 60
+
+func (s *Settings) SetLocationsCache(locations map[string]string, closest string) {
+	setCache(REGIONS_CACHE_KEY, REGIONS_CACHE_TTL_SECONDS, locations)
+	setCache(DEFAULT_REGION_CACHE_KEY, REGIONS_CACHE_TTL_SECONDS, closest)
+}
+
+func (s *Settings) LocationsCache() (map[string]string, string) {
+	locations, err := getCache[map[string]string](REGIONS_CACHE_KEY)
+	if err != nil {
+		return nil, ""
+	}
+	defaultLocation, err := getCache[string](DEFAULT_REGION_CACHE_KEY)
+	if err != nil {
+		return nil, ""
+	}
+	return locations, defaultLocation
+}

internal/settings/settings.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/kirsle/configdir"
 	"github.com/mitchellh/mapstructure"
@@ -44,64 +43,6 @@ func ReadSettings() (*Settings, error) {
 		}
 	}
 	return &Settings{}, nil
-}
-
-const DB_NAMES_CACHE_KEY = "cached_db_names"
-const DB_NAMES_CACHE_TTL_SECONDS = 30 * 60
-const DB_NAMES_CACHE_VALUE_FIELD_NAME = "db_names"
-
-func (s *Settings) SetDbNamesCache(dbNames []string) {
-	setCache(DB_NAMES_CACHE_KEY, DB_NAMES_CACHE_VALUE_FIELD_NAME, DB_NAMES_CACHE_TTL_SECONDS, dbNames)
-}
-
-func (s *Settings) GetDbNamesCache() []string {
-	return getCache(DB_NAMES_CACHE_KEY, DB_NAMES_CACHE_VALUE_FIELD_NAME)
-}
-
-func (s *Settings) InvalidateDbNamesCache() {
-	invalidateCache(DB_NAMES_CACHE_KEY, DB_NAMES_CACHE_VALUE_FIELD_NAME)
-}
-
-const REGIONS_CACHE_KEY = "cached_region_names"
-const REGIONS_CACHE_TTL_SECONDS = 24 * 60 * 60
-const REGIONS_CACHE_VALUE_FIELD_NAME = "region_names"
-
-func (s *Settings) SetRegionsCache(regions []string) {
-	setCache(REGIONS_CACHE_KEY, REGIONS_CACHE_VALUE_FIELD_NAME, REGIONS_CACHE_TTL_SECONDS, regions)
-}
-
-func (s *Settings) GetRegionsCache() []string {
-	return getCache(REGIONS_CACHE_KEY, REGIONS_CACHE_VALUE_FIELD_NAME)
-}
-
-func setCache(cacheKey string, valueFieldName string, ttl int64, value []string) {
-	viper.Set(cacheKey+"."+valueFieldName, value)
-	viper.Set(cacheKey+".expiration_time", time.Now().Unix()+ttl)
-	err := viper.WriteConfig()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error saving settings: ", err)
-	}
-}
-
-func getCache(cacheKey string, valueFieldName string) []string {
-	expirationTime := viper.GetInt64(cacheKey + ".expiration_time")
-	if expirationTime == 0 {
-		return nil
-	}
-	if expirationTime <= time.Now().Unix() {
-		invalidateCache(cacheKey, valueFieldName)
-		return nil
-	}
-	return viper.GetStringSlice(cacheKey + "." + valueFieldName)
-}
-
-func invalidateCache(cacheKey string, valueFieldName string) {
-	viper.Set(DB_NAMES_CACHE_KEY+".expiration_time", 0)
-	viper.Set(DB_NAMES_CACHE_KEY+"."+valueFieldName, []string{})
-	err := viper.WriteConfig()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error saving settings: ", err)
-	}
 }
 
 func (s *Settings) AddDatabase(id string, dbSettings *DatabaseSettings) {

commit 32fb8cbbb2db0bd1044bed6472a5107a1ce0dfff

Fix cache invalidation
Fixes a cache invalidation regression introduced in d75f14f

Fixes #415

Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

internal/settings/cache.go
@@ -1,8 +1,6 @@
 package settings
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
@@ -50,18 +48,8 @@ func getCache[T any](key string) (T, error) {
 	return entry.Data, nil
 }
 
-func invalidateCache(key string) error {
-	configMap := viper.AllSettings()
-	delete(configMap, cacheKey(key))
-	encodedConfig, err := json.MarshalIndent(configMap, "", " ")
-	if err != nil {
-		return err
-	}
-	if err := viper.ReadConfig(bytes.NewReader(encodedConfig)); err != nil {
-		return err
-	}
-	settings.changed = true
-	return nil
+func invalidateCache[T any](key string) error {
+	return setCacheWithExp[T](key, 0, *new(T))
 }
 
 const DB_NAMES_CACHE_KEY = "database_names"
@@ -80,7 +68,7 @@ func (s *Settings) GetDbNamesCache() []string {
 }
 
 func (s *Settings) InvalidateDbNamesCache() {
-	invalidateCache(DB_NAMES_CACHE_KEY)
+	invalidateCache[[]string](DB_NAMES_CACHE_KEY)
 }
 
 const REGIONS_CACHE_KEY = "locations"