ffb874dbfae11c7ff73410604b2928ba1d652106
Glauber Costa <glauber@turso.tech>
2023-05-20 16:38:38 -0400

    change default port

--- internal/cmd/dev_port_flag.go
+++ internal/cmd/dev_port_flag.go
@@ -5,5 +5,5 @@ import "github.com/spf13/cobra"
 var devPort int
 
 func addDevPortFlag(cmd *cobra.Command) {
-	cmd.Flags().IntVar(&devPort, "port", 3030, "the port to which bind the server")
+	cmd.Flags().IntVar(&devPort, "port", 8080, "the port to which bind the server")
 }
