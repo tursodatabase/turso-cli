8a8f70b461e8a2d512cce5835b1b394f784abd75
Athos Couto <athos@chiselstrike.com>
2023-05-23 12:38:01 -0300

    Add shorthand -f to --from-file

--- internal/cmd/from_file_flag.go
+++ internal/cmd/from_file_flag.go
@@ -5,5 +5,5 @@ import "github.com/spf13/cobra"
 var fromFileFlag string
 
 func addFromFileFlag(cmd *cobra.Command, usage string) {
-	cmd.Flags().StringVar(&fromFileFlag, "from-file", "", usage)
+	cmd.Flags().StringVarP(&fromFileFlag, "from-file", "f", "", usage)
 }
