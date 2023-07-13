7cc3f5f278f709fdc9dfaf5f74a533def37d89f9
João Marcos Gris <joaomarcosgris@hotmail.com>
2023-07-13 09:01:44 -0300

    fix: new instructions to transfer

--- internal/cmd/db_transfer.go
+++ internal/cmd/db_transfer.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/chiselstrike/iku-turso-cli/internal"
 	"github.com/chiselstrike/iku-turso-cli/internal/prompt"
-	"github.com/chiselstrike/iku-turso-cli/internal/settings"
 	"github.com/chiselstrike/iku-turso-cli/internal/turso"
 	"github.com/spf13/cobra"
 )
@@ -15,7 +14,7 @@ func init() {
 }
 
 var dbTransferCmd = &cobra.Command{
-	Use:               "transfer database_name org_name",
+	Use:               "db-transfer database_name org_name",
 	Short:             "Transfers database to another organization",
 	Args:              cobra.ExactArgs(2),
 	ValidArgsFunction: dbNameAndOrgArgs,
@@ -31,8 +30,11 @@ var dbTransferCmd = &cobra.Command{
 		if _, err := getDatabase(client, dbName); err != nil {
 			return err
 		}
+		fmt.Printf("To transfer %s database to another organization, all its replicas must be updated.\n", internal.Emph(dbName))
+		fmt.Printf("All your active connections to the DB will be dropped and there will be a short downtime.\n\n")
 
 		ok, err := promptConfirmation(fmt.Sprintf("Are you sure you want to transfer database %s to organization %s?", internal.Emph(dbName), internal.Emph(orgName)))
+
 		if err != nil {
 			return fmt.Errorf("could not get prompt confirmed by user: %w", err)
 		}
@@ -47,12 +49,7 @@ var dbTransferCmd = &cobra.Command{
 }
 
 func transfer(client *turso.Client, dbName, orgName string) error {
-	config, err := settings.ReadSettings()
-	config.InvalidateDatabasesCache()
-
-	if err != nil {
-		return err
-	}
+	invalidateDatabasesCache()
 
 	msg := fmt.Sprintf("Transferring database %s to organization %s", internal.Emph(dbName), internal.Emph(orgName))
 	s := prompt.Spinner(msg)
