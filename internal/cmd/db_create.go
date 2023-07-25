e7e20a4b64cb83cfdbe9e483d1ec3ed2306ca4f8
João Marcos Gris <joaomarcosgris@hotmail.com>
2023-07-25 15:26:43 -0300

    fix: add retry to seed file db create (#573)
    * fix: add retry to seed file db create
    
    * fix: add final error clause

--- internal/cmd/db_create.go
+++ internal/cmd/db_create.go
@@ -215,7 +215,14 @@ var createCmd = &cobra.Command{
 		if waitFlag || dbFile != nil {
 			description = fmt.Sprintf("Waiting for database %s to be ready", internal.Emph(name))
 			spinner.Text(description)
-			if err = client.Instances.Wait(name, instance.Name); err != nil {
+			numberOfRetries := 0
+			for numberOfRetries < 5 {
+				if err = client.Instances.Wait(name, instance.Name); err == nil {
+					break
+				}
+				numberOfRetries++
+			}
+			if err != nil {
 				return err
 			}
 		}
