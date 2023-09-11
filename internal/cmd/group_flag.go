e4b7f48301516354747018c0bd33e1bd17312c27
Athos Couto <athos@turso.tech>
2023-09-11 09:55:36 -0300

    Remove unused flag

--- internal/cmd/group_flag.go
+++ internal/cmd/group_flag.go
@@ -8,7 +8,3 @@ func addGroupFlag(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&groupFlag, "group", "", "create the database in the specified group")
 	cmd.Flags().MarkHidden("group")
 }
-
-func addPersistentGroupFlag(cmd *cobra.Command, description string) {
-	cmd.PersistentFlags().StringVarP(&groupFlag, "group", "g", "", description)
-}
