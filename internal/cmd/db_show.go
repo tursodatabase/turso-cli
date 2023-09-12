59e8310b8555e712493981ec47bf73ecc1ede928
Athos Couto <athos@turso.tech>
2023-09-12 16:24:10 -0300

    Add version field on turso db show if returned from API response

--- internal/cmd/db_show.go
+++ internal/cmd/db_show.go
@@ -83,6 +83,9 @@ var showCmd = &cobra.Command{
 		fmt.Println("Name:          ", db.Name)
 		fmt.Println("URL:           ", getDatabaseUrl(&db))
 		fmt.Println("ID:            ", db.ID)
+		if db.Version != "" {
+			fmt.Println("Version:       ", db.Version)
+		}
 		fmt.Println("Locations:     ", strings.Join(regions, ", "))
 		fmt.Println("Size:          ", humanize.Bytes(dbUsage.Usage.StorageBytesUsed))
 		fmt.Println()
