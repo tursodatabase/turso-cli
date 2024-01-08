ea123c70c0df5334dfa7acd4adbb9f8f10fad9a7
Athos Couto <athos@turso.tech>
2024-01-08 12:42:54 -0300

    Add group transfer cmd

---
+++ internal/cmd/group_transfer.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tursodatabase/turso-cli/internal"
+	"github.com/tursodatabase/turso-cli/internal/flags"
+	"github.com/tursodatabase/turso-cli/internal/prompt"
+	"github.com/tursodatabase/turso-cli/internal/turso"
+)
+
+func init() {
+	groupCmd.AddCommand(groupTransferCmd)
+	flags.AddYes(groupTransferCmd, "Confirms the transfer")
+}
+
+var groupTransferCmd = &cobra.Command{
+	Use:               "transfer [group] [organization]",
+	Short:             "Transfers the group to the specified organization",
+	Args:              cobra.ExactArgs(2),
+	ValidArgsFunction: nil, // TODO
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+		client, err := createTursoClientFromAccessToken(true)
+		if err != nil {
+			return err
+		}
+		group := args[0]
+		org := args[1]
+
+		if _, err := getGroup(client, group); err != nil {
+			return err
+		}
+
+		if yesFlag {
+			return transferGroup(client, group, org)
+		}
+
+		fmt.Printf("Once group %s is transfered, all its locations and databases will belong to organization %s.\n\n", internal.Emph(group), internal.Emph(org))
+		fmt.Println(internal.Warn("Warning:"))
+		fmt.Println("\tHostnames from all databases will be updated to use the new organization name.")
+		fmt.Println("\tOld hostnames will keep working until the database name is reused on the current organization.")
+		fmt.Printf("\tMake sure to update connection URL used by your applications as soon as the transfer is done to avoid disruptions.\n\n")
+
+		ok, err := promptConfirmation("Are you sure you want to do this?")
+		if err != nil {
+			return fmt.Errorf("could not get prompt confirmed by user: %w", err)
+		}
+
+		if !ok {
+			fmt.Println("Group transfer skipped by the user.")
+			return nil
+		}
+
+		return transferGroup(client, group, org)
+	},
+}
+
+func transferGroup(client *turso.Client, group, organization string) error {
+	msg := fmt.Sprintf("Transfering group %s to organization %s", internal.Emph(group), internal.Emph(organization))
+	s := prompt.Spinner(msg)
+	defer s.Stop()
+
+	if err := client.Groups.Transfer(group, organization); err != nil {
+		return fmt.Errorf("error transfering group: %w", err)
+	}
+
+	s.Stop()
+	fmt.Printf("✔  Success! Group %s was transfered successfully to organization %s\n", internal.Emph(group), internal.Emph(organization))
+	return nil
+}
