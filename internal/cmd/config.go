3352d27961a1dfb5db47c81d6f66ad9e0f0f4a5b
Athos Couto <athos@turso.tech>
2024-01-24 19:36:55 -0300

    Add 'turso config set token' cmd

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -12,6 +12,7 @@ func init() {
 	rootCmd.AddCommand(configCmd)
 	configCmd.AddCommand(configSetCmd)
 	configSetCmd.AddCommand(configSetAutoUpdateCmd)
+	configSetCmd.AddCommand(configSetTokenCmd)
 }
 
 var configCmd = &cobra.Command{
@@ -50,6 +51,31 @@ var configSetAutoUpdateCmd = &cobra.Command{
 		settings.SetLastUpdateCheck(0) // trigger an update
 		fmt.Println("Autoupdate set to", internal.Emph(value))
 
+		return nil
+	},
+}
+
+var configSetTokenCmd = &cobra.Command{
+	Use:   "token <jwt>",
+	Short: "Configure the token used by turso",
+	Args:  cobra.ExactArgs(1),
+	ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return []string{}, cobra.ShellCompDirectiveNoFileComp
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+		settings, err := settings.ReadSettings()
+		if err != nil {
+			return fmt.Errorf("failed to read settings: %w", err)
+		}
+
+		token := args[0]
+		if !isJwtTokenValid(token) {
+			return fmt.Errorf("invalid token")
+		}
+
+		settings.SetToken(token)
+		fmt.Println("Token set succesfully.")
 		return nil
 	},
 }
