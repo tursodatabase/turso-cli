8942f26e3d7a539750929da0370257fdd4590969
Avinash Sajjanshetty <opensource@avi.im>
2024-02-22 17:16:38 +0530

    Update cmd description for claim

--- internal/flags/claim.go
+++ internal/flags/claim.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 var claimFlag []string
 
 func AddClaim(cmd *cobra.Command) {
-	cmd.Flags().StringSliceVar(&claimFlag, "claim", nil, "Custom claims in JSON format")
+	cmd.Flags().StringSliceVar(&claimFlag, "claim", nil, "list of database claims to be added to the token")
 }
 
 func Claim() []string {
