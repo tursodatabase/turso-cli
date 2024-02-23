86abe5a39405a47a3f70430a273ce3177157b782
Avinash Sajjanshetty <640792+avinassh@users.noreply.github.com>
2024-02-23 14:27:52 +0530

    Update internal/flags/claim.go
    Co-authored-by: Athos <athos.couto@outlook.com>

--- internal/flags/claim.go
+++ internal/flags/claim.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 var attachFlag []string
 
-func AddAttachClaim(cmd *cobra.Command) {
+func AddAttachClaims(cmd *cobra.Command) {
 	cmd.Flags().StringSliceVar(&attachFlag, "attach", nil, "list of database names with attach claim to be added to the token")
 }
 
