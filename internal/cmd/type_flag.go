fc7a294fe139c1536d6b8025d438f45f08628241
Piotr Jastrzebski <piotr@chiselstrike.com>
2024-02-23 09:22:25 +0100

    Add --type to db create
    Signed-off-by: Piotr Jastrzebski <piotr@chiselstrike.com>

---
+++ internal/cmd/type_flag.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var typeFlag string
+
+func addTypeFlag(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&typeFlag, "type", "regular", "Type of the database to create. Possible values: regular, schema. Default: regular.")
+	cmd.RegisterFlagCompletionFunc("type", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return []string{"regular", "schema"}, cobra.ShellCompDirectiveNoFileComp
+	})
+}
