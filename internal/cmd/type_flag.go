0f23af92147888ae7398fc0d80acb12fbeff0972
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-04-17 10:08:26 -0400

    chore: remove repetition of default value in `--type` flag help text

--- internal/cmd/type_flag.go
+++ internal/cmd/type_flag.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 var typeFlag string
 
 func addTypeFlag(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&typeFlag, "type", "regular", "Type of the database to create. Possible values: regular, schema. Default: regular.")
+	cmd.Flags().StringVar(&typeFlag, "type", "regular", "Type of the database to create. Possible values: regular, schema.")
 	cmd.RegisterFlagCompletionFunc("type", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return []string{"regular", "schema"}, cobra.ShellCompDirectiveNoFileComp
 	})
