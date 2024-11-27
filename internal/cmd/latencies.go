bbda7a9064122b52a06012a2d3445449c81a16cc
Glauber Costa <glauber@turso.tech>
2024-11-27 15:05:24 -0500

    remove latency flag
    Less useful than before given decreased edge focus, and doesn't work
    on AWS.

--- internal/cmd/latencies.go
+++
@@ -1,9 +0,0 @@
-package cmd
-
-import "github.com/spf13/cobra"
-
-var latencyFlag bool
-
-func addLatencyFlag(cmd *cobra.Command) {
-	cmd.Flags().BoolVarP(&latencyFlag, "show-latencies", "l", false, "Display latencies from your current location to each of Turso's locations")
-}
